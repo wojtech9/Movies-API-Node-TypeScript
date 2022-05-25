26b80f5db93d9f8d43a2f43b738f329066a0c31e
Wojtek <wojtek.tech9@gmail.com>
2022-05-25 02:18:10 +0200

    Project setup

---
+++ src/api/index.ts
@@ -0,0 +1,26 @@
+import express from 'express';
+import cors from 'cors';
+import path from 'path';
+const app = express();
+require('dotenv').config();
+const PORT = process.env.PORT;
+
+// routes import
+
+import favorites from './routes/Favorites';
+
+// basic middlewares, CORS - accepts requests from all origin
+
+app.use(cors());
+app.use('/', express.static(path.join(__dirname, 'static')));
+app.use(express.json());
+
+// routes handling
+
+app.use('/favorites', favorites);
+
+// server listener
+
+app.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}...`);
+});
